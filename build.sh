fdf54b5464dcb68c189cef6a8a849fe99d1ecd5c
Abdussamet Kocak <abdus@abdus.dev>
2026-03-09 20:46:23 +0300

    chore(ci): cache docker layers via build.sh flags

--- build.sh
+++ build.sh
@@ -3,8 +3,16 @@
 set -e
 cd "$(dirname "$0")"
 
-docker build -t vips-wasm .
-docker run --rm -v "$(pwd)/wasm:/src/wasm" vips-wasm -c '
+IMAGE_TAG="${VIPS_WASM_IMAGE:-vips-wasm}"
+DOCKER_BUILD_FLAGS="${DOCKER_BUILD_FLAGS:-}"
+
+if [ -n "$DOCKER_BUILD_FLAGS" ]; then
+  docker buildx build $DOCKER_BUILD_FLAGS -t "$IMAGE_TAG" .
+else
+  docker build -t "$IMAGE_TAG" .
+fi
+
+docker run --rm -v "$(pwd)/wasm:/src/wasm" "$IMAGE_TAG" -c '
   VIPS_CFLAGS=$(pkg-config --static --cflags vips | sed "s/-pthread//g")
   VIPS_LIBS=$(pkg-config --static --libs vips | sed "s/-pthread//g")
   emcc /src/wasm/thumbnail.c /src/wasm/stubs.c \
@@ -15,4 +23,5 @@ docker run --rm -v "$(pwd)/wasm:/src/wasm" vips-wasm -c '
 '
 
 cp wasm/thumbnail.wasm vips-thumbnail.wasm
+cp wasm/thumbnail.wasm govips/thumbnail.wasm
 echo "Built: vips-thumbnail.wasm ($(du -sh vips-thumbnail.wasm | cut -f1))"
